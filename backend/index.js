aef98f8714f7b433be3f47ae0fe24cdb2f9a3638
Haripriyaa <mailtohari1224@gmail.com>
2024-09-12 13:08:57 +0530

    Server Created

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const port = 10000;
+
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log('listening on port '+port);
+})
