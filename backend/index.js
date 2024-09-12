84b3d41957c8096a2afd10ae1b94d4536d58ee2d
Haripriyaa <mailtohari1224@gmail.com>
2024-09-12 14:37:38 +0530

    testAPI created

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
-const port = 10000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('api working');
+    })
+
 
 app.listen(port, ()=>{
     console.log('listening on port '+port);
